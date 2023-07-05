34f2f72d6e863ba53ac724dc404d3b770d02d208
Christopher Mills <christopher.mills-cic-uk@ibm.com>
2023-07-05 19:33:33 +0100

    nice working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=20")
+      .then((response) => {
+        const { results } = response.data;
+        setPokemonData(results);
+      })
+      .catch((error) => {
+        console.error("Error fetching Pokémon data:", error);
+      });
+  }, []);
+
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: <button className="carousel-nav">Previous</button>,
+    nextArrow: <button className="carousel-nav">Next</button>,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokémon Carousel</h1>
+      <Slider {...sliderSettings}>
+        {pokemonData.map((pokemon) => (
+          <div key={pokemon.name}>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url
+                .split("/")[6]
+                .replace(/[^0-9]/g, "")}.png`}
+              alt={pokemon.name}
+            />
+            <p>{pokemon.name}</p>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 }
